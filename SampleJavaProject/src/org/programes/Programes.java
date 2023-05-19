37dc50c5fefb2773d04cd3a5ddccaf3079a938f8
thangarajd <130234373+thangarajd@users.noreply.github.com>
2023-05-19 06:14:46 +0530

    Add files via upload

---
+++ SampleJavaProject/src/org/programes/Programes.java
@@ -0,0 +1,24 @@
+package org.programes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Programes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+		Map<String,Integer> hm = new HashMap<>();
+		hm.put("First", 1);
+		hm.put("Second", 2);
+		hm.put("Third", 3);
+		hm.put("Four", 4);
+		hm.put("Five", 5);
+	
+		for(Map.Entry<String, Integer> e: hm.entrySet()) {
+			
+			System.out.println("Key = "+ e.getKey() + ", Value = " + e.getValue());
+		}
+	}
+
+}
---
+++ SampleJavaProject/src/org/programes/SampleJavaClass.java
@@ -0,0 +1,10 @@
+package org.programes;
+
+public class SampleJavaClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
