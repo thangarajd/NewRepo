fc9d205d2412b33802d0d889785c7ccae740e194
thangarajd <thangarajd>
2023-05-19 06:35:51 +0530

    first commit

--- SampleJavaProject/src/org/programes/SampleJavaClass.java
+++ SampleJavaProject/src/org/programes/SampleJavaClass.java
@@ -6,6 +6,9 @@ public class SampleJavaClass {
 		System.out.println("Hello world");
 		System.out.println("Hello");
 		System.out.println("FirstProgram");
+		System.out.println("Second day code");
+		System.out.println("Hello This is second day code");
+	
 
 	}
 
