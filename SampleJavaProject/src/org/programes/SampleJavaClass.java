1873e6984e8ad287d85791f2e4ec671571bcf935
thangarajd <thangarajd>
2023-05-19 06:26:22 +0530

    my first commit

--- SampleJavaProject/src/org/programes/SampleJavaClass.java
+++ SampleJavaProject/src/org/programes/SampleJavaClass.java
@@ -4,6 +4,8 @@ public class SampleJavaClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello");
+		System.out.println("FirstProgram");
 
 	}
 
